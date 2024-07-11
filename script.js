3739b3580f333db8562f299b9bf0b4573bb742d3
nodichao <josephhenrinodichao@gmail.com>
2024-07-11 16:37:29 +0200

    responsive features

---
+++ script.js
@@ -0,0 +1,7 @@
+let hamburger = document.querySelector('.hamburger');
+let menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click',()=>{
+     menu.classList.toggle('mobile-menu');
+})
+
